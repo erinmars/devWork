e53e85ac0bb4feaca7c23f1bd8ecc602308a69bb
Dougherty, Diane <Diane.Dougherty@Pearson.com>
2017-10-20 17:41:38 -0700

    commit xcode starter files

--- dev_tuts/ng_heros_tutorial/src/app/app.module.ts
+++ dev_tuts/ng_heros_tutorial/src/app/app.module.ts
@@ -1,17 +1,28 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here.
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-    ],
-  declarations: [ 
-    AppComponent 
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
   ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
